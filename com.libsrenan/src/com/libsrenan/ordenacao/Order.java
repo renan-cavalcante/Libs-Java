b41863244082eeef37354026d91229c1f49a5f2f
renan-cavalcante <renancggama@gmail.com>
2023-09-17 12:09:11 -0300

    pilhas

--- com.libsrenan/src/com/libsrenan/ordenacao/Order.java
+++ com.libsrenan/src/com/libsrenan/ordenacao/Order.java
@@ -3,7 +3,7 @@ package com.libsrenan.ordenacao;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public  class  order<T> {
+public  class  Order<T extends Comparable<T>>  {
 	
 	
 	public static <T extends Comparable<T>> List<T> bubbleSort(List<T> list) {
@@ -14,9 +14,7 @@ public  class  order<T> {
 			continuar = false;
 			for(int j = 0; j < tamanho-i-1; j++) {
 				if(  list.get(j).compareTo( list.get(j+1)) > 0) {
-					T aux = list.get(j);
-					list.set(j, list.get(j + 1));
-					list.set(j + 1, aux);
+					trocar(list, j, j+1);
 					continuar = true;
 				}
 			}
@@ -59,5 +57,49 @@ public  class  order<T> {
 		 }
 		
 	}
+	
+	public static <T extends Comparable<T>> List<T> quickSort (List<T> lista,int inicio, int fim){
+		if(fim < inicio) {
+			int meio = dividir(lista, inicio, fim );
+			quickSort(lista, inicio, meio -1);
+			quickSort(lista, meio + 1, fim);
+		}
+		return lista;
+	}
+
+	private static <T extends Comparable<T>> int dividir(List<T> lista, int inicio, int fim) {
+		T fixo = lista.get(inicio);
+		int esquerda = inicio + 1;
+		int direita = fim;
+		
+		while(esquerda <= direita) {
+			while(esquerda <= direita && lista.get(esquerda).compareTo(fixo) <= 0 ) {
+				esquerda++;
+			}
+			
+			while(esquerda <= direita && lista.get(direita).compareTo(fixo) > 0 ) {
+				direita--;
+			}
+			
+			if(esquerda < direita) {
+				trocar(lista, esquerda, direita);
+				esquerda++;
+				direita--;
+			}
+			
+		}
+		
+		trocar(lista, inicio, direita);
+		return direita;
+	}
+
+	private static <T extends Comparable<T>> void trocar(List<T> lista, int i, int j) {
+		T aux = lista.get(i);
+		lista.set(i, lista.get(j));
+		lista.set(j, aux);
+		
+		
+	}
+
 
 }
